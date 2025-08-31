e68a95d51a12eab6ecce16fd91a64f06cd4738e2
Salvadore Oranja <s.oranja.s@gmail.com>
2025-08-31 18:00:45 +0400

    [commit-1] chore: init snip-vault (vite+react+tailwind v4)

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="min-h-dvh grid place-items-center">
+      <div className="text-sm text-zinc-500">snip-vault</div>
+    </div>
+  );
+}
